e2db8a8c488f838170a8bfb5d57aefdfc16d97f1
Sam Limpic <samlimpic@gmail.com>
2021-06-05 20:29:23 -0600

    updated

--- app.js
+++ app.js
@@ -1,14 +1,92 @@
-let hello = document.getElementById('hello')
 let helloInt = setInterval(() => {
     flash('hello', 300)
 }, 1200);
 
+const header = document.getElementById('header')
+const headerName = document.getElementById('header-name')
+const headerIcon = document.getElementById('header-icon')
+const contact = document.getElementById('contact')
+const contactIcon = document.getElementById('contact-icon')
+const cardRow = document.getElementById('card-row')
+const footer = document.getElementById('footer')
+const footerText = document.getElementById('footer-text')
+const footerIcon = document.getElementById('footer-icon')
+const card = Array.from(document.getElementsByClassName('card'))
+const cardBody = Array.from(document.getElementsByClassName('card-body'))
+const cardTitle = Array.from(document.getElementsByClassName('card-title'))
+const cardSubtitle = Array.from(document.getElementsByClassName('card-subtitle'))
+const cardText = Array.from(document.getElementsByClassName('card-text'))
+const star = Array.from(document.getElementsByClassName('star'))
+const icon = Array.from(document.getElementsByClassName('icon'))
+
+const cards = {
+    keepr: {
+        title: 'Fellowship',
+        subtitle: 'Vue3 - SCSS - C# - SQL',
+        text: 'A social media site populated with characters from The Lord of the Rings'
+    },
+    tavern: {
+        title: 'The Tavern',
+        subtitle: 'Vue3 - SCSS - Node.js - MongoDB',
+        text: 'An interactive character-builder for Dungeons & Dragons 5th Edition'
+    },
+    bug: {
+        title: 'Bug Catcher',
+        subtitle: 'Vue3 - SCSS - Node.js - MongoDB',
+        text: 'A mock message board to post, comment on, and release bugs'
+    },
+    bill: {
+        title: 'Bill Boards',
+        subtitle: 'Vue3 - SCSS - Node.js - MongoDB',
+        text: 'A task manager inspired by Trello, utilizing FillMurray to its upmost'
+    },
+    smith: {
+        title: 'Master Smith',
+        subtitle: 'Javascript - SCSS',
+        text: 'A task-list blacksmith simulation utilizing custom art assets'
+    },
+    miner: {
+        title: 'Dungeoneer',
+        subtitle: 'Javascript',
+        text: 'A multi-stage click-counting adventure utilizing custom art assets'
+    }
+}
+
+function helloWorld(str, num) {
+    let message = document.getElementById('hello')
+    // @ts-ignore
+    document.getElementById('start').disabled = true
+    document.getElementById('start').classList.add('disable')
+    setTimeout(() => {
+        clearInterval(helloInt)
+        message.classList.remove('code-text')
+        message.classList.add('text-black')
+        for (let i = 0; i < str.length; i++) {
+            setTimeout(function () {
+            if (message.innerText[0] == '_') {
+                message.innerText = ''
+                }
+            message.innerText += str[i]
+            }, num * i);
+    }
+    }, num);
+    setTimeout(() => {
+        let flashInt = setInterval(() => {
+            flash('hello', num)
+        }, num * 4);
+        setTimeout(() => {
+            clearInterval(flashInt)
+            load(num/3)
+        }, num*16);
+    }, (num*str.length)-num);
+}
+
 function flash(id, num) {
     let elem = document.getElementById(id)
     elem.classList.remove('text-black')
-    elem.classList.add('text-white')
+    elem.classList.add('code-text')
         setTimeout(function() {
-            elem.classList.remove('text-white')
+            elem.classList.remove('code-text')
             elem.classList.add('text-black')
         }, num * 2)
 }
@@ -20,79 +98,268 @@ function message(id, str, num) {
                 message.innerText += str[i]
             }, num * i);
         }
+    setTimeout(() => {
+        message.innerText = str.replaceAll('_', ' ')
+    }, (num*str.length));
 }
 
+function load(num) {
+    document.getElementById('intro').classList.add('hidden')
+    remove()
+    buildHeader(num)
+}
 
-function helloWorld(str, num) {
-    let message = document.getElementById('hello')
-    // @ts-ignore
-    document.getElementById('start').disabled = true
-    document.getElementById('start').classList.add('disable')
+function remove() {
+    header.classList.remove('modern', 'bg-secondary', 'hidden')
+    headerName.classList.remove('font-xxl', 'text-light', 'text-shadow')
+    headerIcon.classList.remove('font-xxl', 'text-shadow', 'text-primary')
+    contact.classList.remove('btn', 'btn-outline-primary', 'bg-transparent', 'border-0', 'font-xxl')
+    contactIcon.classList.remove('hidden', 'text-shadow')
+    headerName.innerText = ''
+
+    document.getElementById('showtime').classList.remove('modern')
+    removeCard('keepr')
+    removeCard('tavern')
+    removeCard('bug')
+    removeCard('bill')
+    removeCard('smith')
+    removeCard('miner')
+
+    footer.classList.remove('modern', 'bg-secondary')
+    document.getElementById('footer-text-1').innerText = ''
+    document.getElementById('footer-text-2').innerText = ''
+    footerText.classList.remove('font-lg', 'text-light', 'text-center', 'text-shadow')
+    footerIcon.classList.remove('text-warning')
+}
+
+function removeCard(str) {
+    document.getElementById(str + '-card').classList.remove('shadow', 'bg-light', 'rounded')
+    document.getElementById(str + '-card').classList.add('border-success', 'square')
+    document.getElementById(str + '-body').classList.add('code-back')
+    if (document.getElementById(str + '-star')) {
+        document.getElementById(str + '-star').classList.remove('star', 'font-xs')
+    }
+    document.getElementById(str + '-icon').classList.remove('icon', 'font-xxl')
+    document.getElementById(str + '-title').innerText = ''
+    document.getElementById(str + '-title').classList.remove('font-xxl')
+    document.getElementById(str + '-subtitle').innerText = ''
+    document.getElementById(str + '-subtitle').classList.remove('text-muted', 'font-md')
+    document.getElementById(str + '-text').innerText = ''
+    document.getElementById(str + '-text').classList.remove('font-lg')
+}
+
+function buildHeader(num) {
+    document.getElementById('skip-plus').classList.remove('hidden')
+    document.getElementById('skip').classList.add('hidden')
+    message('header-name', 'Sam_Limpic', num)
+    const str = 'Sam_Limpic'
     setTimeout(() => {
-        clearInterval(helloInt)
-        message.classList.remove('text-white')
-        message.classList.add('text-black')
-        for (let i = 0; i < str.length; i++) {
-            setTimeout(function () {
-            if (message.innerText[0] == '_') {
-                message.innerText = ''
-                }
-            message.innerText += str[i]
+        buildBody(num / 2, 'keepr')
+    }, str.length*num);
+}
+
+function buildBody(num, str) {
+    document.getElementById(str).classList.remove('hidden')
+    document.getElementById(str).classList.add('code-text', 'pt-2')
+    let message = document.getElementById(str + '-title')
+    let text = cards[str].title.replaceAll(' ', '_')
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(function () {
+            message.innerText += text[i]
         }, num * i);
     }
-    }, num);
     setTimeout(() => {
-        let flashInt = setInterval(() => {
-            flash('hello', num/2)
-        }, num * 3);
+        message.innerText = text.replaceAll('_', ' ')
+        message = document.getElementById(str + '-subtitle')
+        text = cards[str].subtitle.replaceAll(' ', '_')
+        for (let i = 0; i < text.length; i++) {
+            setTimeout(function () {
+                message.innerText += text[i]
+            }, num * i);
+        }
         setTimeout(() => {
-            clearInterval(flashInt)
-            load()
-        }, num*12);
-    }, (num*str.length)-num);
+            message.innerText = text.replaceAll('_', ' ')
+            message = document.getElementById(str + '-text')
+            text = cards[str].text.replaceAll(' ', '_')
+            for (let i = 0; i < text.length; i++) {
+                setTimeout(function () {
+                    message.innerText += text[i]
+                }, num/2 * i);
+            }
+            setTimeout(() => {
+                message.innerText = text.replaceAll('_', ' ')
+                setTimeout(() => {
+                    if (document.getElementById(str + '-star')) {
+                    document.getElementById(str + '-star').classList.remove('hidden')
+                    document.getElementById(str + '-star').classList.add('code-text', 'star', 'font-xs')
+                    }
+                    setTimeout(() => {
+                        document.getElementById(str + '-icon')  .classList.remove('hidden')
+                        document.getElementById(str + '-icon').classList.add('code-text', 'icon', 'font-xxl')
+                        setTimeout(() => {
+                            switchBuild(num, str)
+    }, num);
+    }, num);
+    }, num);
+    }, (num/2*text.length));
+    }, (num*text.length));
+    }, (num*text.length));
 }
 
-function load() {
-    document.getElementById('intro').classList.add('hidden')
-    document.body.classList.remove('bg-white')
-    document.getElementById('showtime').classList.remove('hidden')
-    document.getElementById('header').classList.remove('hidden')
-    document.getElementById('footer').classList.remove('hidden')
-}
-
-function expand(str) {
+function switchBuild(num, str) {
     switch(str) {
-        case str === 'keepr':
-            window.open()
-        break;
-        case str == 'tavern':
-            window.open()
+        case 'keepr':
+        buildBody(num, 'tavern')
             break;
-        case str == 'bug':
-            window.open('https://bug-catcher-vue.herokuapp.com/#/')
+        case 'tavern':
+        buildBody(num, 'bug')
             break;
-        case str == 'bill':
-            window.open('https://bill-boards.herokuapp.com/#/')
+        case 'bug':
+        buildBody(num, 'bill')
             break;
-        case str == 'smith':
-            window.open('https://samlimpic.github.io/master-smith/')
+        case 'bill':
+        buildBody(num, 'smith')
             break;
-        case str == 'miner':
-            window.open('https://samlimpic.github.io/dungeon-miner/')
+        case 'smith':
+        buildBody(num, 'miner')
             break;
-        default:
+        default: buildFooter(num*2)
+    }
 }
-    
+
+function buildFooter(num) {
+    footer.classList.remove('hidden')
+    footerText.classList.remove('hidden')
+    message('footer-text-1', 'Made_With_', num)
+    const str = 'Made_With_'
+    setTimeout(() => {
+        footerIcon.classList.remove('hidden')
+        setTimeout(() => {
+            message('footer-text-2', 'By_Sam_Limpic', num)
+            const str = 'By_Sam_Limpic'
+            setTimeout(() => {
+                buildStyle(num*4)
+    }, str.length*num);
+    }, num);
+    }, str.length*num);
+}
+
+function buildStyle(num) {
+        setTimeout(() => {
+                for (let i = 0; i < card.length; i++) {
+                    setTimeout(function () {
+                        card[i].classList.remove('code-text', 'border-success', 'square', 'pt-2');
+                        card[i].classList.add('bg-light', 'text-dark', 'shadow', 'rounded');
+                        setTimeout(function () {
+                            cardBody[i].classList.remove('code-text', 'code-back');
+                        }, num * i);
+                    }, num * i);
+                }
+        setTimeout(() => {
+            document.body.classList.remove('code')
+        setTimeout(() => {
+            header.classList.add('modern')
+            headerName.classList.add('font-xxl')
+        setTimeout(() => {
+            for (let i = 0; i < card.length; i++) {
+                setTimeout(function () {
+                    cardTitle[i].classList.add('font-xxl', 'modern', 'underline');
+                    setTimeout(function () {
+                        cardSubtitle[i].classList.add('font-md', 'modern', 'text-muted');
+                        setTimeout(function () {
+                            cardText[i].classList.add('font-lg', 'modern');
+                            setTimeout(function () {
+                                icon[i].classList.remove('code-text');
+                                icon[i].classList.add('text-primary');
+                }, num * i);
+                }, num * i);
+                }, num * i);
+                }, num * i);
+            }
+        setTimeout(() => {
+            header.classList.add('bg-secondary')
+            headerName.classList.add('text-light')
+        setTimeout(() => {
+            footerText.classList.add('text-light')
+            footer.classList.add('bg-secondary')
+        setTimeout(() => {
+            for (let i = 0; i < star.length; i++) {
+                setTimeout(function () {
+                    star[i].classList.remove('code-text');
+                    star[i].classList.add('text-warning');
+                }, num * i);
+            }
+        setTimeout(() => {
+            footer.classList.add('modern')
+            footer.classList.add('text-center')
+            footerText.classList.add('font-lg')
+        setTimeout(() => {
+            footerIcon.classList.add('text-warning')
+        setTimeout(() => {
+            headerName.classList.add('text-shadow')
+            footerText.classList.add('text-shadow')
+        setTimeout(() => {
+            document.getElementById('loading').classList.add('hidden')
+        setTimeout(() => {
+            headerIcon.classList.remove('hidden')
+            headerIcon.classList.add('font-xxl')
+        setTimeout(() => {
+            headerIcon.classList.add('text-primary')
+            headerIcon.classList.add('text-shadow')
+        setTimeout(() => {
+            document.getElementById('skip').classList.add('hidden')
+            document.getElementById('skip-plus').classList.add('hidden')
+        setTimeout(() => {
+            contactIcon.classList.remove('hidden')
+            contact.classList.remove('hidden')
+            contact.classList.add('btn', 'btn-outline-primary', 'bg-transparent', 'border-0')
+        setTimeout(() => {
+            contact.classList.add('font-xxl')
+            contactIcon.classList.add('text-shadow')
+        setTimeout(() => {
+        
+    }, num);
+    }, num);
+    }, num);
+    }, num);
+    }, num);
+    }, num);
+    }, num);
+    }, num);
+    }, num);
+    }, star.length*num);
+    }, card.length*2*num);
+    }, num);
+    }, num);
+    }, num);
+    }, num);
+    }, card.length*2*num);
+    }, num);
 }
 
 function skip() {
+    let hidden = document.getElementsByClassName('hidden')
+    while(hidden.length > 0){
+    hidden[0].classList.remove('hidden');
+    }
     document.getElementById('intro').classList.add('hidden')
-    document.body.classList.remove('bg-white')
-    document.getElementById('showtime').classList.remove('hidden')
-    document.getElementById('header').classList.remove('hidden')
-    document.getElementById('footer').classList.remove('hidden')
     document.getElementById('skip').classList.add('hidden')
+    document.getElementById('skip-plus').classList.add('hidden')
+    document.getElementById('loading').classList.add('hidden')
+    document.body.classList.remove('code')
+}
 
+window.onload = function () {
+  let reload = sessionStorage.getItem("reload");
+  if (reload) {
+    sessionStorage.removeItem("reload");
+    skip();
+  }
+}
+
+function reloadPage() {
+  sessionStorage.setItem("reload", "true");
+  document.location.reload();
 }
 
 flash('hello', 300)
